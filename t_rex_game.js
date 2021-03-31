ec4967aba17540ad351bedeae4df22a2dfd4f507
Carlos Hughes <carlos_hughes@hotmail.com>
2021-03-30 22:16:56 -0600

    Update t_rex_game.js

--- t_rex_game.js
+++ t_rex_game.js
@@ -1,3 +1,11 @@
+/*
+Developed by
+Carlos Ivan Hughes Corona
+Computer Systems Engineer
+Full-Stack
+2017
+*/
+
 document.addEventListener('keydown', function(evento){
 	if (evento.keyCode === 32){
 		console.log("salta");
